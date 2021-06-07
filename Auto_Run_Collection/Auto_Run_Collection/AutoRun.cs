7e5352c9c5dd3619cafc668c7701de374f2bbc87
Diệp Gia Hữu <83325530+huudg@users.noreply.github.com>
2021-06-07 10:55:00 +0700

    V1.1

--- Auto_Run_Collection/Auto_Run_Collection/AutoRun.cs
+++ Auto_Run_Collection/Auto_Run_Collection/AutoRun.cs
@@ -16,20 +16,35 @@ namespace Auto_Run_Collection
     {
         CancellationTokenSource tokenSource = new CancellationTokenSource();
         DateTime itime = DateTime.Now;
+        int data = 0;
+        Task timerTask;
         public AutoRun()
         {
-            InitializeComponent();          
+            InitializeComponent();
+            textBox3.Enabled = false;
+            button2.Enabled = false;
         }
 
         private void button1_Click(object sender, EventArgs e)
-        {
-            int time = Convert.ToInt32(textBox4.Text);
-            Task timerTask = RunPeriodically(runCollection, TimeSpan.FromSeconds(time), tokenSource.Token);
+        {            
+            int time = Convert.ToInt32(txtTime.Text);
+            if (comboBox1.Text == "Day")
+                timerTask = RunPeriodically(runCollection, TimeSpan.FromDays(time), tokenSource.Token);
+            if (comboBox1.Text == "Hours")
+                timerTask = RunPeriodically(runCollection, TimeSpan.FromHours(time), tokenSource.Token);
+            if (comboBox1.Text == "Minute")
+                timerTask = RunPeriodically(runCollection, TimeSpan.FromMinutes(time), tokenSource.Token);
+            if (comboBox1.Text == "Seconds")
+                timerTask = RunPeriodically(runCollection, TimeSpan.FromSeconds(time), tokenSource.Token);
+            button2.Enabled = true;
+            button1.Enabled = false;
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             tokenSource.Cancel();
+            button2.Enabled = false;
+            button1.Enabled = true;
         }
         async Task RunPeriodically(Action action, TimeSpan interval, CancellationToken token)
         {
@@ -45,9 +60,26 @@ namespace Auto_Run_Collection
             ps.CreateNoWindow = true;
             ps.FileName = "cmd.exe";
             ps.WindowStyle = ProcessWindowStyle.Normal;
-            //ps.Arguments = @"/k cd C:\Users\diepg\Postman\Collection && newman run Input_Output.postman_collection.json -d in_out.csv -r htmlextra";
-            ps.Arguments = @"/k cd " + @textBox1.Text.Replace(@"\\", @"\") + " && newman run " + textBox2.Text +" -d " + textBox3.Text +" -r htmlextra";
+            if (data==1)
+                ps.Arguments = @"/k cd " + @textBox1.Text.Replace(@"\\", @"\") + " && newman run " + textBox2.Text +" -d " + textBox3.Text +" -r htmlextra";
+            else
+                ps.Arguments = @"/k cd " + @textBox1.Text.Replace(@"\\", @"\") + " && newman run " + textBox2.Text + " -r htmlextra";
             Process.Start(ps);
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (data == 0)
+            {
+                data = 1;
+                textBox3.Enabled = true;
+            }
+            else
+            {
+                data = 0;
+                textBox3.Enabled = false;
+                textBox3.Clear();
+            }
         }
     }
 }
